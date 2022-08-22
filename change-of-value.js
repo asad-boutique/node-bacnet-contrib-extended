1621acb9cb282202ae2d67531d805e49f58b115b
asad-boutique <95596533+asad-boutique@users.noreply.github.com>
2022-08-22 17:03:41 +1000

    feat: bacnet change of value

--- change-of-value.js
+++ change-of-value.js
@@ -14,11 +14,24 @@ module.exports = function(RED) {
 
 
 
-      this.name = config.name
+      this.name = config.name;
+
+      this.address = config.address;
+
+      this.inputType = config.inputType;
+
+      this.bacnetId = config.bacnetId;
+      
+      this.COVtimeout = config.COVtimeout;
+
+
 
       const node = this;
 
       node.status({ fill: 'green', shape: 'dot', text: 'active' })
+
+
+      
 
 
 
@@ -27,7 +40,7 @@ module.exports = function(RED) {
       node.on('input', function(msg) {
 
 
-
+/*
 
         console.log("test 000 - change of value");
 
@@ -74,7 +87,7 @@ module.exports = function(RED) {
 
           console.log("test 222");
 
-
+*/
 
 
 
@@ -87,20 +100,23 @@ module.exports = function(RED) {
 
 
 
+      var randNum = Math.floor(Math.random() * 7100);
+      var communicationPort = 47808 + randNum;
 
-
-
-
-
+      var interface = "0.0.0.0";
+      var broadcastAddress = "255.255.255.255";
+      var apduTimeout = 7000;
+      var reuseAddr = true;
+      //var transportClosedDuration = 90000;
 
 
 
       var bacnetClient = new Bacnet({ 
-        port: 47812,                          // Use BAC4 as communication port
-        interface: '0.0.0.0',          // Listen on a specific interface
-        broadcastAddress: '255.255.255.255',  // Use the subnet broadcast address
-        apduTimeout: 4000,
-        reuseAddr: true                     
+        port: communicationPort,                         
+        interface: interface,          // Listen on a specific interface
+        broadcastAddress: broadcastAddress,  // Use the subnet broadcast address
+        apduTimeout: apduTimeout,
+        reuseAddr: reuseAddr                     
        });
 
 
@@ -120,6 +136,35 @@ module.exports = function(RED) {
 
 
       
+      
+
+      
+
+      // var address = this.document.getElementById('input-address');
+      // var inputType = this.document.getElementById('input-inputType');
+      // var bacnetId = this.document.getElementById('input-bacnetId');
+
+      
+
+
+
+      // var address = "192.168.20.242";
+      // var inputType = 3;
+      // var bacnetId = 8;
+
+      var address = node.address;
+      var inputType = Number(node.inputType);
+      var bacnetId = Number(node.bacnetId);
+      var COVtimeout = Number(node.COVtimeout);
+
+
+      console.log("address ---> " + address);
+      console.log("inputType ---> " + inputType);
+      console.log("bacnetId ---> " + bacnetId);
+
+
+
+
       bacnetClient.on('covNotifyUnconfirmed', (data) => {
 
         console.log('000 Received COV: ' + data);
@@ -131,25 +176,50 @@ module.exports = function(RED) {
         console.log('333 Received COV: ' + str2);
 
 
+        // if(Number(str.monitoredObjectId['type']) == inputType && Number(str.monitoredObjectId['instance']) == bacnetId){
+
+        //   node.send(data);
+
+        // }
+
+
         
         node.send(data);
 
       });
+    
 
-      var address = "192.168.20.242";
-      var inputType = 3;
-      var bacnetId = 8;
 
-      bacnetClient.subscribeCov(address, {type: inputType, instance: bacnetId}, 85, false, false, 0, (err) => {
-        console.log('222 subscribeCOV' + err);
-      });
+      SubcribeCOV(address, inputType, bacnetId, COVtimeout);
       
 
 
 
+      function SubcribeCOV(address, inputType, bacnetId, COVtimeout){
+
+        bacnetClient.subscribeCov(address, {type: inputType, instance: bacnetId}, 85, false, false, 0, (err) => {
+          
+  
+          if(err){
+            console.log('subscribeCOV error -> ', err);
+          }
+  
+          else{
+  
+            setTimeout(() => {
+              SubcribeCOV(address, inputType, bacnetId, COVtimeout);
+              }, COVtimeout);
+  
+          }
+  
+        });
+
+      }
+      
 
 
 
+   
 
 
 
@@ -171,7 +241,7 @@ module.exports = function(RED) {
 
 
   }
-  RED.nodes.registerType("change-of-value",ChangeOfValue);
+  RED.nodes.registerType("change-of-value", ChangeOfValue);
 
 
 
