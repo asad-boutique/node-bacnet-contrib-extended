98b861abe768fdcfab996a0d8ebe9e89fe00353f
asad-boutique <95596533+asad-boutique@users.noreply.github.com>
2022-09-07 11:28:32 +1000

    Fix: Read Properties Multiple fix

--- read-single-point.js
+++ read-single-point.js
@@ -221,7 +221,17 @@ module.exports = function(RED) {
     
           var k1 = object.values[j].id.toString();
           
-          var v1 = object.values[j].value[0].value;
+          // console.log("k1 --> ", k1)
+
+          var v1 = null;
+
+          if (object.values[j].value[0]?.value){
+
+             v1 = object.values[j].value[0].value;
+
+             }
+
+          // console.log("v1 --> ", v1)
           
           var k1str = ConvertPropertyIdWithString(k1);
           
@@ -241,172 +251,26 @@ module.exports = function(RED) {
 
 
       function ConvertPropertyIdWithString(id){
-    
+
+        
         var convertedValue = " PPPP ";
-        
-        if(id == 0){
-            convertedValue = "ACKED_TRANSITIONS";
-        }
-        
-        else if(id == 4){
-            convertedValue = "ACTIVE_TEXT";
-        }
-        
-        else if(id == 6){
-            convertedValue = "ALARM_VALUE";
-        }
-        
-        else if(id == 15){
-            convertedValue = "CHANGE_OF_STATE_COUNT";
-        }
-        
-        else if(id == 16){
-            convertedValue = "CHANGE_OF _STATE_TIME";
-        }
-        
-        else if(id == 17){
-            convertedValue = "NOTIFICATION_CLASS";
-        }
-        
-        else if(id == 22){
-            convertedValue = "COV_INCREMENT";
-        }
+
+        var arrayOfKeys = Object.keys(Bacnet.enum.PropertyIdentifier);
+        var arrayOfValues = Object.values(Bacnet.enum.PropertyIdentifier);
     
-        else if(id == 25){
-            convertedValue = "DEADBAND";
-        }
+        // console.log("arrayOfValues --> ", arrayOfValues);
     
-        else if(id == 28){
-            convertedValue = "DESCRIPTION";
-        }
-        
-        else if(id == 31){
-            convertedValue = "DEVICE_TYPE";
-        }
-        
-        else if(id == 33){
-            convertedValue = "ELAPSED_ACTIVE_TIME";
-        }
-        
-        else if(id == 35){
-            convertedValue = "EVENT_ENABLE";
-        }
-        
-        else if(id == 36){
-            convertedValue = "EVENT_STATE";
-        }
+        // console.log("id -->", id);
     
-        else if(id == 45){
-            convertedValue = "HIGH_LIMIT";
-        }
-        
-        else if(id == 46){
-            convertedValue = "INACTIVE_TEXT";
-        }
+        var indexOfS = arrayOfValues.indexOf(parseInt(id, 10));
     
-        else if(id == 52){
-            convertedValue = "LIMIT_ENABLE";
-        }
+        // console.log("indexOfS -->", indexOfS);
     
-        else if(id == 59){
-            convertedValue = "LOW_LIMIT";
-        }
+        //console.log("key -> ", arrayOfKeys[indexOfS]);
     
-        else if(id == 65){
-            convertedValue = "MAX_PRES_VALUE";
-        }
+        convertedValue = arrayOfKeys[indexOfS];
     
-        else if(id == 69){
-            convertedValue = "MIN_PRES_VALUE";
-        }
         
-        else if(id == 72){
-            convertedValue = "NOTIFY_TYPE";
-        }
-        
-        else if(id == 75){
-            convertedValue = "OBJECT_IDENTIFIER";
-        }
-        
-        else if(id == 77){
-            convertedValue = "OBJECT_NAME";
-        }
-        
-        else if(id == 79){
-            convertedValue = "OBJECT_TYPE";
-        }
-        
-        else if(id == 81){
-            convertedValue = "OUT_OF_SERVICE";
-        }
-        
-        else if(id == 84){
-            convertedValue = "POLARITY";
-        }
-        
-        else if(id == 85){
-            convertedValue = "PRESENT_VALUE";
-        }
-        
-        else if(id == 103){
-            convertedValue = "RELIABILITY";
-        }
-    
-        else if(id == 106){
-            convertedValue = "RESOLUTION";
-        }
-        
-        else if(id == 111){
-            convertedValue = "STATUS_FLAGS";
-        }
-        
-        else if(id == 113){
-            convertedValue = "TIME_DELAY";
-        }
-        
-        else if(id == 114){
-            convertedValue = "TIME_OF_ACTIVE_TIME_RESET";
-        }
-        
-        else if(id == 115){
-            convertedValue = "TIME_OF_STATE_COUNT_RESET";
-        }
-    
-        else if(id == 117){
-            convertedValue = "UNITS";
-        }
-    
-        else if(id == 118){
-            convertedValue = "UPDATE_INTERVAL";
-        }
-        
-        else if(id == 130){
-            convertedValue = "EVENT_TIME_STAMPS";
-        }
-        
-        else if(id == 351){
-            convertedValue = "EVENT_MESSAGE_TEXTS";
-        }
-        
-        else if(id == 352){
-            convertedValue = "EVENT_MESSAGE_TEXTS_CONFIGS";
-        }
-        
-        else if(id == 353){
-            convertedValue = "EVENT_DETECTION_ENABLE";
-        }
-        
-        else if(id == 354){
-            convertedValue = "EVENT_ALGORITHM_INHIBIT";
-        }
-        
-        else if(id == 355){
-            convertedValue = "EVENT_ALGORITHM_INHIBIT_REF";
-        }
-        
-        else if(id == 356){
-            convertedValue = "TIME_DELAY_NORMAL";
-        }
         
         return convertedValue;
         
